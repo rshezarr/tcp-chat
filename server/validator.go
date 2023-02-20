d87fad592e22d0085fcd1268054ca2569bf49a93
rshezarr <rshezarr@gmail.com>
2023-02-20 16:22:54 +0600

    add: message validator

--- server/validator.go
+++ server/validator.go
@@ -1,1 +1,11 @@
 package server
+
+// Chceking messages and names function
+func isValidStr(str string) bool {
+	for _, w := range str {
+		if w != ' ' && w != '\n' {
+			return true
+		}
+	}
+	return false
+}
