e63dedc09771ece775c1e6a9a8a119b4c03e8acf
rshezarr <rshezarr@gmail.com>
2023-02-20 16:28:49 +0600

    update: main

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
+	"tcp-chat/server"
+)
 
+func main() {
+	port := "8989"
+	args := os.Args[1:]
+	if len(args) == 1 {
+		port = args[0]
+	} else if len(args) != 0 {
+		fmt.Println("[USAGE]: ./TCPChat $port")
+		return
+	}
+
+	if err := server.NewHub().Run(port); err != nil {
+		log.Printf("Error: %v", err)
+	}
 }
