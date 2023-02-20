8de20638b2e661d39fedb61dc83f6f9fa2803830
rshezarr <rshezarr@gmail.com>
2023-02-20 16:24:24 +0600

    add: taken name validator

--- server/validator.go
+++ server/validator.go
@@ -1,6 +1,11 @@
 package server
 
-// Chceking messages and names function
+import (
+	"net"
+	"strings"
+)
+
+// Checking messages and names function
 func isValidStr(str string) bool {
 	for _, w := range str {
 		if w != ' ' && w != '\n' {
@@ -10,7 +15,7 @@ func isValidStr(str string) bool {
 	return false
 }
 
-// Chceking port function
+// Checking port function
 func isValidPort(port string) bool {
 	for _, v := range port {
 		if v < '0' || v > '9' {
@@ -18,4 +23,17 @@ func isValidPort(port string) bool {
 		}
 	}
 	return true
+}
+
+// Check for taken names
+func (h *Hub) isTakenName(conn net.Conn, tempName string) bool {
+	h.Lock()
+	for _, u := range h.users {
+		if u == strings.TrimSpace(tempName) {
+			conn.Write([]byte("Name has already taken! Try again"))
+			return true
+		}
+	}
+	h.Unlock()
+	return false
 }
