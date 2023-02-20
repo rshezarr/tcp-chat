d1f7fb32e6e87ca61796676d475046843dee28ac
rshezarr <rshezarr@gmail.com>
2023-02-20 16:24:59 +0600

    add: const vars

--- server/server.go
+++ server/server.go
@@ -5,6 +5,12 @@ import (
 	"sync"
 )
 
+const (
+	maxConn        = 10
+	userJoinedChat = " has joined our chat...\n"
+	userLeftChat   = " has left our chat...\n"
+)
+
 type Hub struct {
 	sync.Mutex
 	users       map[net.Conn]string
